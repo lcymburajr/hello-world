2eba5696f144743d1c6eaf95b8167354d6e9bc56
Lawrence Cymbura <lcymburajr@ljcdesign.com>
2017-01-29 19:07:25 -0500

    install ansible

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "Install python-software-properties"
+sudo apt-get install software-properties-common
+
+echo "install ansible repository"
+sudo add-apt-repository ppa:ansible/ansible
+
+echo "update"
+sudo apt-get update
+
+echo "install ansible"
+sudo apt-get install ansible -y
